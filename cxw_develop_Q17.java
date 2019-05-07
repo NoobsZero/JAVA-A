4566cf9d266bf5925a08f9c1c2a1d349bf3c1396
870628995@qq.com <870628995@qq.com>
2019-05-07 14:59:31 +0800

    delete ras

---
+++ cxw_develop_Q17.java
@@ -0,0 +1,14 @@
+package cn.kgc;
+
+public class cxw_develop_Q17 {
+	// 从下列选项中选择两个正确的答案放在 line 3 中（C E）
+	private double d1 = 1.0;
+
+	// insert code here ~~ line 3
+	/*
+	 * C:private class InnerOne { double methoda() { return d1; } }
+	 */
+	/*
+	 * E:abstract class InnerOne{ public abstract double methoda(); }
+	 */
+}
